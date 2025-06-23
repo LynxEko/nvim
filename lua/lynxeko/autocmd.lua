fb2ae3ff6d80256b99178645d58a0e91f801f369
Erik Villarreal Gallardo <Erik.Villareal@feverup.com>
2025-06-23 11:11:02 +0200

    fix diagnostic movement and add more gotos

--- lua/lynxeko/autocmd.lua
+++ lua/lynxeko/autocmd.lua
@@ -7,12 +7,14 @@ autocmd("LspAttach", {
     group = lynxeko_group,
     callback = function(e)
         local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gD", function() vim.lsp.buf.declaration() end, opts)
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
         vim.keymap.set("n", "<C-h>", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
     end,
 })
