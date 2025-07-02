fe04f40c317252c203f0ef9e1183e54f06addaa7
Erik Villarreal Gallardo <Erik.Villareal@feverup.com>
2025-07-02 11:40:45 +0200

    add grep string search

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -9,6 +9,7 @@ return {
         local builtin = require("telescope.builtin")
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+        vim.keymap.set("n", "<leader>fs", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
         vim.keymap.set("n", "<leader>fw", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
