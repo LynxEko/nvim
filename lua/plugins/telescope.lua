0c84959147185d6ec8227f2ed1ab9ea1c09c3a95
Erik Villarreal Gallardo <Erik.Villareal@feverup.com>
2025-07-03 15:37:11 +0200

    add way to search selection

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,12 @@ return {
         vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
         vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
         vim.keymap.set("n", "<leader>fs", function() builtin.grep_string({ search = vim.fn.input("Grep > ") }) end)
+        vim.keymap.set("v", "<leader>fs", function()
+            local mode = vim.api.nvim_get_mode().mode
+            local opts = (mode == "v" or mode == "V" or mode == "\22") and { type = mode } or vim.empty_dict() -- \22 is the escaped version of ctrl-v
+            local selection = vim.fn.getregion(vim.fn.getpos("."), vim.fn.getpos("v"), opts)
+            builtin.grep_string({ search = selection[1] })
+        end)
         vim.keymap.set("n", "<leader>fw", function()
             local word = vim.fn.expand("<cword>")
             builtin.grep_string({ search = word })
