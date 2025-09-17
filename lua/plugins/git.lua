3dc4510475c6b73053af9b3c9b64863a018dd5bd
Erik Villarreal Gallardo <Erik.Villareal@feverup.com>
2025-09-17 12:55:37 +0200

    add octo for PR reviews

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -16,4 +16,27 @@ return {
             })
         end,
     },
+    {
+        "pwntester/octo.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            local octo = require("octo")
+            octo.setup()
+
+            vim.keymap.set("n", "<leader>gp", function()
+                local pr_number = vim.fn.input("PR number > ")
+                vim.cmd.Octo({ "pr", "edit", pr_number })
+            end, { desc = "Open GitHub issue/PR by number" })
+            vim.keymap.set(
+                "n",
+                "<leader>go",
+                function() vim.cmd.Octo({ "pr", "list" }) end,
+                { desc = "Open GitHub issues/PRs" }
+            )
+        end,
+    },
 }
