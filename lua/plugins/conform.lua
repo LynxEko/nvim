74051be0a7c7f5e7b36e82539a944dd956fabe97
Erik Villarreal Gallardo <Erik.Villareal@feverup.com>
2025-10-29 09:54:03 +0100

    typescript autotag

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,29 +1,36 @@
 return {
-    "stevearc/conform.nvim",
-    opts = {},
-    config = function()
-        local conform = require("conform")
-        conform.setup({
-            formatters_by_ft = {
-                lua = { "stylua" },
-                rust = { "rustfmt", lsp_format = "fallback" },
-                python = { "black" },
-                javascript = { "prettierd" },
-                typescript = { "prettierd" },
-                css = { "prettierd" },
-                scss = { "prettierd" },
-                html = { "prettierd" },
-            },
-            -- log_level = vim.log.levels.DEBUG,
-        })
+    {
+        "stevearc/conform.nvim",
+        opts = {},
+        config = function()
+            local conform = require("conform")
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    rust = { "rustfmt", lsp_format = "fallback" },
+                    python = { "black" },
+                    javascript = { "prettierd" },
+                    typescript = { "prettierd" },
+                    css = { "prettierd" },
+                    scss = { "prettierd" },
+                    html = { "prettierd" },
+                },
+                -- log_level = vim.log.levels.DEBUG,
+            })
 
-        conform.formatters.stylua = {
-            prepend_args = { "--indent-type=Spaces", "--collapse-simple-statement=FunctionOnly" },
-        }
+            conform.formatters.stylua = {
+                prepend_args = { "--indent-type=Spaces", "--collapse-simple-statement=FunctionOnly" },
+            }
 
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*",
-            callback = function(args) require("conform").format({ bufnr = args.buf }) end,
-        })
-    end,
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                pattern = "*",
+                callback = function(args) require("conform").format({ bufnr = args.buf }) end,
+            })
+        end,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        opts = {},
+        config = function() require("nvim-ts-autotag").setup() end,
+    },
 }
